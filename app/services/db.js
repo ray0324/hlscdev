6f0ed67c413802782f011e986bf6977d53865668
ray0324 <ray0324@foxmail.com>
2018-10-13 12:46:11 +0800

    update

--- app/services/db.js
+++ app/services/db.js
@@ -1,7 +1,7 @@
 const Database = require('warehouse');
 const path = require('path');
 
-const DB_PATH = path.resolve('/fixtures/db.json');
+const DB_PATH = path.resolve('./fixtures/db.json');
 console.log(DB_PATH);
 const db = new Database({ path: DB_PATH });
 
